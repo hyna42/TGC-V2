b1b8f926cd67a6cc18c434d9dba49d7988871326
Amadou <bylo@duck.com>
2024-11-18 23:24:17 +0100

    feat : implement fetching ads on RecentAds page

--- frontend/src/components/AdCard.tsx
+++ frontend/src/components/AdCard.tsx
@@ -1,24 +1,34 @@
+import { Link } from "react-router-dom";
 
 export type AdCardProps = {
+  id: number;
   title: string;
-  imgUrl: string;
+  pictures: string;
   price: number;
-  link: string;
+  category?: {
+    id: number;
+    title: string;
+  };
+  tags?: [{ id: number; name: string[] }];
+  createdAt?: Date;
+  description?: string;
+  location?: string;
+  owner?: string;
+  // link: string | "#";
 };
 
-
-const AdCard  = ({ title, imgUrl, price, link }:AdCardProps) => {
- 
+const AdCard = ({ title, pictures, price, category, id }: AdCardProps) => {
   return (
     <>
       <div className="ad-card-container">
-        <a className="ad-card-link" href={link}>
-          <img className="ad-card-image" src={imgUrl} />
+        <Link className="ad-card-link" to={`/ad/${id}`}>
+          <img className="ad-card-image" src={pictures} />
           <div className="ad-card-text">
             <div className="ad-card-title">{title}</div>
-            <div className="ad-card-price">{price} €</div>
+            <div className="ad-card-price">| {price} €</div>
           </div>
-        </a>
+          <p className="ad-card-category">{category?.title} </p>
+        </Link>
       </div>
     </>
   );
--- frontend/src/components/RecentAds.tsx
+++ frontend/src/components/RecentAds.tsx
@@ -1,52 +1,57 @@
 import { useEffect, useState } from "react";
-import AdCard, { AdCardProps } from "./AdCard";
+// import AdCard, { AdCardProps } from "./AdCard";
 import axios from "axios";
+import AdCard, { AdCardProps } from "./AdCard";
 
 const RecentAds = () => {
-  const ads: AdCardProps[] = [
-    {
-      title: "Table",
-      imgUrl: "/public/images/table.webp",
-      price: 120,
-      link: "/ads/table",
-    },
-    {
-      title: "Dame-jeanne",
-      imgUrl: "/public/images/dame-jeanne.webp",
-      price: 75,
-      link: "/ads/dame-jeanne",
-    },
-    {
-      title: "Vide-poche",
-      price: 4,
-      imgUrl: "/public/images/vide-poche.webp",
-      link: "/ads/vide-poche",
-    },
-    {
-      title: "Vaisselier",
-      price: 900,
-      imgUrl: "/public/images/vaisselier.webp",
-      link: "/ads/vaisselier",
-    },
-    {
-      title: "Bougie",
-      price: 8,
-      imgUrl: "/public/images/bougie.webp",
-      link: "/ads/bougie",
-    },
-    {
-      title: "Porte-magazine",
-      price: 45,
-      imgUrl: "/public/images/porte-magazine.webp",
-      link: "/ads/porte-magazine",
-    },
-  ];
+  // const ads: AdCardProps[] = [
+  //   {
+  //     title: "Table",
+  //     imgUrl: "/public/images/table.webp",
+  //     price: 120,
+  //     link: "/ads/table",
+  //   },
+  //   {
+  //     title: "Dame-jeanne",
+  //     imgUrl: "/public/images/dame-jeanne.webp",
+  //     price: 75,
+  //     link: "/ads/dame-jeanne",
+  //   },
+  //   {
+  //     title: "Vide-poche",
+  //     price: 4,
+  //     imgUrl: "/public/images/vide-poche.webp",
+  //     link: "/ads/vide-poche",
+  //   },
+  //   {
+  //     title: "Vaisselier",
+  //     price: 900,
+  //     imgUrl: "/public/images/vaisselier.webp",
+  //     link: "/ads/vaisselier",
+  //   },
+  //   {
+  //     title: "Bougie",
+  //     price: 8,
+  //     imgUrl: "/public/images/bougie.webp",
+  //     link: "/ads/bougie",
+  //   },
+  //   {
+  //     title: "Porte-magazine",
+  //     price: 45,
+  //     imgUrl: "/public/images/porte-magazine.webp",
+  //     link: "/ads/porte-magazine",
+  //   },
+  // ];
 
+  const [ads, setAds] = useState<AdCardProps[]>([]);
   const [total, setTotal] = useState(0);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/ads");
+        const response = await axios.get<AdCardProps[]>(
+          "http://localhost:3000/ads"
+        );
+        if (response.data) setAds(response.data);
         console.log(response.data);
       } catch (error) {
         console.log("error fetching ads", error);
@@ -61,12 +66,14 @@ const RecentAds = () => {
         <p>Prix total : {total} €</p>
         <section className="recent-ads">
           {ads.map((ad) => (
-            <div key={ad.title}>
+            <div key={ad.id}>
               <AdCard
+                id={ad.id}
                 title={ad.title}
-                imgUrl={ad.imgUrl}
+                pictures={ad.pictures}
                 price={ad.price}
-                link={ad.link}
+                // link={ad.id.toString()}
+                category={ad.category}
               />
               <button
                 className="button"
