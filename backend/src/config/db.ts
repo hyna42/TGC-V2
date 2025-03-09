f97dbb9ac948bdee97992d2f9f1a87edbb3e825b
hyna <bylo@duck.com>
2025-03-09 01:57:35 +0100

    feat : add postgres and adminer services

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,13 +3,19 @@ import { Ad } from "../entities/Ad";
 import { Category } from "../entities/Category";
 import { Tag } from "../entities/Tag";
 import { Picture } from "../entities/Picture";
+import "dotenv/config";
 
 export const datasource = new DataSource({
-  type: "better-sqlite3",
-  database: "../../good_corner.sqlite",
+  type: "postgres",
+  database: process.env.DB_NAME as string,
+  host: process.env.DB_HOST as string,
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USER as string,
+  password: process.env.DB_PASSWORD as string,
   entities: [Ad, Category, Tag, Picture],
   synchronize: true,
   logging: ["error", "query"],
-  // migrations: ["migrations/*.ts"],
-  // migrationsTableName: "migrations",
+
 });
+
+// uninstall better-sqlite3
