15f7b221b8de9f13b493fac974a9090bfa0cc6f1
hyna <bylo@duck.com>
2025-03-23 00:03:23 +0100

    feat(clean): remove orphan images from uploads folder based on DB picture table

--- img/clean.ts
+++ img/clean.ts
@@ -5,6 +5,10 @@
  * 2. Mapper la liste des urls dans le dossier uploads
  * 3. Supprimer les urls dans uploads qui ne sont pas dans la db (Piture)
  */
+const path = require("path");
+const fs = require("fs");
+const directoryPath = path.join(__dirname, "uploads");
+
 import pg from "pg";
 const { Client } = pg;
 import "dotenv/config";
@@ -20,8 +24,39 @@ import "dotenv/config";
 
   try {
     await client.connect();
-    const res = await client.query("SELECT * FROM picture");
-    console.log(res.rows);
+    const response = await client.query("SELECT * FROM picture");
+    if (!response.rowCount) return;
+    // 1 liste des urls presents en DB
+    const dbUrls = response.rows.map((item) => item.url.slice(5));
+    console.log("dbURLs", dbUrls);
+
+    //2 liste des urls dans le dossier uploads du service img
+    fs.readdir(
+      directoryPath,
+      function (err: NodeJS.ErrnoException, files: string[]) {
+        if (err) {
+          return console.log("Unable to scan directory: " + err);
+        }
+        console.log("uploadsURLs ==> ", files);
+        //supprimer le fichier s'il nest pas dans la bd
+        let count = 0;
+        files.forEach(function (file) {
+          if (!dbUrls.includes(file)) {
+            count++;
+            fs.unlink(
+              `${directoryPath}/${file}`,
+              (err: NodeJS.ErrnoException) => {
+                if (err) {
+                  console.error(`Error removing file: ${err}`);
+                  return;
+                }
+              }
+            );
+          }
+        });
+        console.log(`🗑️ ${count} File has been successfully removed.`);
+      }
+    );
   } catch (err) {
     console.error("Error connecting to db", err);
   } finally {
