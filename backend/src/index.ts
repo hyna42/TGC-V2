30b079262152ac583048f5527f877624e00c287f
hyna <bylo@duck.com>
2025-03-26 01:17:43 +0100

    feat(auth): implement JWT cookie auth with login, logout and context support

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,13 +7,22 @@ import AdResolver from "./resolvers/AdResolver";
 import CategoryResolver from "./resolvers/CategoryResolver";
 import TagResolver from "./resolvers/TagResolver";
 import UserResolver from "./resolvers/UserResolver";
+import "dotenv/config";
+import jwt, { Secret } from "jsonwebtoken";
+import * as cookie from "cookie";
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY manquant dans .env");
+  }
   await datasource.initialize();
 
   const schema = await buildSchema({
     resolvers: [AdResolver, CategoryResolver, TagResolver, UserResolver],
-    
+
+    authChecker: ({ context }) => {
+      return !!context.email; // Retourne true si l'email existe, sinon false
+    },
   });
 
   const server = new ApolloServer({
@@ -22,6 +31,28 @@ const start = async () => {
 
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
+    context: async ({ req, res }) => {
+      if (req.headers.cookie) {
+        const cookies = cookie.parse(req.headers.cookie as string);
+        console.log("Cookies in Headers ===> ", cookies);
+        if (cookies.token !== undefined) {
+          try {
+            const payload: any = jwt.verify(
+              cookies.token,
+              process.env.JWT_SECRET_KEY as Secret
+            );
+            console.log("payload in context", payload);
+            if (payload) {
+              console.log("payload was found and returned to resolver");
+              return { email: payload.email, res };
+            }
+          } catch (error) {
+            console.error("JWT verification failed: ", error);
+          }
+        }
+      }
+      return { res };
+    },
   });
 
   console.log(`🚀 Server ready at: ${url}`);
--- backend/src/resolvers/UserResolver.ts
+++ backend/src/resolvers/UserResolver.ts
@@ -1,7 +1,9 @@
-import { Arg, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
 import { User } from "../entities/User";
 import UserInput from "../inputs/UserInput";
 import * as argon2 from "argon2";
+import "dotenv/config";
+import jwt, { Secret } from "jsonwebtoken";
 
 @Resolver(User)
 class UserResolver {
@@ -11,7 +13,7 @@ class UserResolver {
     const password = await argon2.hash(data.hashedPassword);
 
     const newUser = User.create({
-      ...data,
+      email: data.email,
       hashedPassword: password,
     });
     await newUser.save();
@@ -19,12 +21,40 @@ class UserResolver {
   }
 
   //connexion : login
-  // @Mutation(() => User)
-  // async login(@Arg("data") id: number) {
-  //   return User.findOneBy({ id });
-  // }
+  @Mutation(() => String)
+  async login(@Arg("data") data: UserInput, @Ctx() context: any) {
+    const user = await User.findOneBy({ email: data.email });
+    if (!user) throw new Error("Identifiants invalides");
+    const isPasswordCorrect = await argon2.verify(
+      user?.hashedPassword,
+      data.hashedPassword
+    );
+    if (!isPasswordCorrect) throw new Error("Identifiants incorrects");
+
+    //générer un JWT token
+    if (!process.env.JWT_KEY) {
+      throw new Error("JWT_KEY manquant dans .env");
+    }
+    const token = jwt.sign(
+      { email: user.email },
+      process.env.JWT_KEY as Secret
+    );
+
+    //ajouter un cookie dans la réponse http
+    context.res.setHeader("Set-Cookie", `token=${token}; Secure; HttpOnly`);
+
+    return "login ok";
+  }
 
   //deconnexion : logout
+  @Mutation(() => String)
+  async logout(@Ctx() context: any) {
+    context.res.setHeader(
+      "Set-Cookie",
+      `token=; Secure; HttpOnly;expires=${new Date(Date.now()).toUTCString()}`
+    );
+    return "logged out";
+  }
 
   //deleteUser == supprimer mon compte
   // @Mutation(() => String)
