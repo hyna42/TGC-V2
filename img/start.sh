3b4abd5a40b8399c3151b10ef7357cdef8967875
hyna <bylo@duck.com>
2025-05-26 00:04:04 +0200

    chore(upload): add API_URL config, vite proxy for local dev, and simplify Dockerfile/start.sh

--- img/start.sh
+++ img/start.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 # Initialiser les fichiers dans /app/uploads si c'est vide
-if [ -z "$(ls -A /app/uploads)" ]; then
-  echo "📦 Initialisation des fichiers d'uploads..."
-  cp -r /app/uploads_seed/* /app/uploads/
-fi
+
 
 # Démarrer le service cron en arrière-plan
 crond -b
